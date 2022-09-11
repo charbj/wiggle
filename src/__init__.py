bb299139a1ecfcb542b662afcf8a830cf494c30d
Charles Bayly-Jones <39799152+charbj@users.noreply.github.com>
2022-09-11 10:00:54 +1000

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -25,17 +25,17 @@ class _MyAPI(BundleAPI):
         # ones listed in bundle_info.xml (without the leading and
         # trailing whitespace), and create and return an instance of the
         # appropriate class from the ``tool`` module.
-        if ti.name == "Wiggle":
+        if ti.name == "wiggle":
             from . import tool
-            return tool.Wiggle(session, ti.name)
+            return tool.wiggle(session, ti.name)
         raise ValueError("trying to start unknown tool: %s" % ti.name)
 
     @staticmethod
     def get_class(class_name):
         # class_name will be a string
-        if class_name == "Wiggle":
+        if class_name == "wiggle":
             from . import tool
-            return tool.Wiggle
+            return tool.wiggle
         raise ValueError("Unknown class name '%s'" % class_name)
 
 # Create the ``bundle_api`` object that ChimeraX expects.
